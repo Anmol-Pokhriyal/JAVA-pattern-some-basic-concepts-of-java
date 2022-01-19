fc066065b002f1aa5071a33f4826bb6cc6595a3b
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-01-19 15:58:55 +0530

    Add files via upload

---
+++ BigDecimalAndBigInteger.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BigDecimalAndBigInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+//		double a=0.03;
+//		double b=0.03;
+//		double c=a+b;
+//		System.out.println(c);      
+
+//		BigDecimal a=new BigDecimal("0.03");
+//		BigDecimal b=new BigDecimal("0.05");
+//		BigDecimal c=b.subtract(a);
+//		System.out.println(c);      
+
+		
+		BigInteger a=new BigInteger("5");
+		BigInteger b=new BigInteger("7765515444987798777987979874448943212121816548887712346");
+		BigInteger c=b.add(a);
+
+		
+		System.out.println(c);
+
+	}
+
+}
---
+++ DataType.java
@@ -0,0 +1,18 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0;
+		double d=6325.2254;
+		float pi=225.35f;
+		char ch='D';
+		boolean status=false;
+		String name="Ram";
+	
+		
+		System.out.print("Happyyyy birthday");
+
+	}
+
+}
